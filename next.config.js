3a8434fe8916eb21334b1323b06fddbd925abfb2
DESKTOP-HP\Admin <vitalgol>
2025-04-06 22:16:40 +0300

    change config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  images: {
-    unoptimized: true, // нужно, если ты используешь <Image>
-  },
-  // Укажи basePath, если деплоишь в подкаталог (например: /my-project)
   basePath: "/MyCV",
   assetPrefix: "/MyCV/",
+  // images: {
+  //   unoptimized: true, // нужно, если ты используешь <Image>
+  // },
+  // Укажи basePath, если деплоишь в подкаталог (например: /my-project)
 };
 
 module.exports = nextConfig;
